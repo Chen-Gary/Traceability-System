1d7924bad30a3d0b211b2790e0aaceeb43555f07
Gary Chen <chenhhgary@gmail.com>
2022-04-30 22:47:48 +0800

    convert_rawMaterial2battery

--- composer/logic.js
+++ composer/logic.js
@@ -23,3 +23,27 @@ async function createNewRawMaterial(create) {
     // Add the rawMaterial to asset registry
     await assetRegistry.add(rawMaterial);
 }
+
+
+/**
+ * Convert: raw material to battery.
+ * New battery instance is created, basing on raw materials.
+ * @param {org.traceabilitysystem.Convert_rawMaterial2battery} convert_rawMaterial2battery
+ * @transaction
+ */
+
+async function convertRawMaterial2battery(convert_rawMaterial2battery) {
+    let assetRegistry = await getAssetRegistry('org.traceabilitysystem.Battery');
+
+    var factory = getFactory();
+
+    var battery = factory.newResource('org.traceabilitysystem', 'Battery', convert_rawMaterial2battery.batchID);
+
+    battery.model = convert_rawMaterial2battery.model;
+    battery.quality = convert_rawMaterial2battery.quality;
+    battery.creationDate = convert_rawMaterial2battery.creationDate;
+    battery.rawMaterialBatchID = convert_rawMaterial2battery.rawMaterialBatchID;
+    battery.owner = convert_rawMaterial2battery.owner;
+
+    await assetRegistry.add(battery);
+}
